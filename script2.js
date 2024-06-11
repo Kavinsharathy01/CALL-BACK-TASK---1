7c843d6f87465f6f24f33cd6393792d6b0731c92
Kavinsharathy01 <166894740+Kavinsharathy01@users.noreply.github.com>
2024-06-12 00:03:53 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,28 @@
+
+
+function my(){
+  setTimeout(()=>
+    console.log("10"),2000);
+    setTimeout(()=>
+      console.log("9"),3000);
+        setTimeout(()=>
+        console.log("8"),4000);
+          setTimeout(()=>
+          console.log("7"),5000);
+            setTimeout(()=>
+            console.log("6"),6000);
+              setTimeout(()=>
+              console.log("5"),7000);
+                 setTimeout(()=>
+                 console.log("4"),8000);
+                    setTimeout(()=>
+                    console.log("3"),9000);
+                      setTimeout(()=>
+                      console.log("2"),10000);
+                        setTimeout(()=>
+                        console.log("1"),11000);
+                          setTimeout(()=>
+                          console.log("HAPPY INDENPENDENCE DAY"),12000);
+}
+  
+my()
